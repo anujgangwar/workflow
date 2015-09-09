44f460f04bed9d43bc38c32b288ff14de081eb71
Anuj Gangwar <anuj.gangwar2007@gmail.com>
2015-09-10 03:22:22 +0530

    generate enum class file test 2

--- lib/workflow/adapters/active_record.rb
+++ lib/workflow/adapters/active_record.rb
@@ -54,6 +54,7 @@ module Workflow
           workflow_without_scopes(&specification)
           states = workflow_spec.states.values
           count = 1
+          byebug
           File.open("WorkflowState.rb","w+") do |f|
             f.write("class WorkflowState\n")
             states.each do |state|
