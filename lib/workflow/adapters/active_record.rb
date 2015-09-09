9364eccb901f3a5f0a1c5e10a320be64f2f859a8
Anuj Gangwar <anuj.gangwar2007@gmail.com>
2015-09-10 03:56:22 +0530

    byebug: load_workflow_state

--- lib/workflow/adapters/active_record.rb
+++ lib/workflow/adapters/active_record.rb
@@ -9,6 +9,7 @@ module Workflow
 
       module InstanceMethods
         def load_workflow_state
+          byebug
           read_attribute(self.class.workflow_column)
         end
 
@@ -55,7 +56,6 @@ module Workflow
           states = workflow_spec.states.values
           generate_enum_file(states)
           states.each do |state|
-            byebug
             define_singleton_method("with_#{state}_state") do
               where("#{table_name}.#{self.workflow_column.to_sym} = ?", state.to_s)
             end
